4d9c7a3d47df65b00d8344bfbd46d594f5605dcd
Murilo <naosoulolicon@email.com>
2026-04-17 21:48:14 -0300

    corrigindo estrutura do projeto

---
+++ Revista Eletronica/js/script.js
@@ -0,0 +1,16 @@
+let index = 0;
+
+const slides = document.querySelector('.slides');
+const totalSlides = document.querySelectorAll('.slide').length;
+
+function nextSlide() {
+  index++;
+
+  if (index >= totalSlides) {
+    index = 0;
+  }
+
+  slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+setInterval(nextSlide, 4000);
