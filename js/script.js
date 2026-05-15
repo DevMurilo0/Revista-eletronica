8c8ff881b69b9f986c2fd3f7476e0ca7084dd1fd
Murilo <naosoulolicon@email.com>
2026-05-15 07:53:19 -0300

    design

--- js/script.js
+++ js/script.js
@@ -14,3 +14,22 @@ function nextSlide() {
 }
 
 setInterval(nextSlide, 4000);
+
+
+
+window.addEventListener('load', function() {
+  const limitarScroll = () => {
+    const organizadores = document.querySelector('.organizadores');
+    const acessoFolha = document.querySelector('.acesso-folha');
+    
+    if (organizadores && acessoFolha) {
+      const maxScroll = acessoFolha.offsetTop + acessoFolha.offsetHeight - window.innerHeight;
+      
+      if (window.scrollY > maxScroll) {
+        window.scrollTo(0, maxScroll);
+      }
+    }
+  };
+  
+  window.addEventListener('scroll', limitarScroll);
+});
